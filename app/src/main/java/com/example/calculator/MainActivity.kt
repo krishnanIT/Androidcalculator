15d6fa108dbcff05fe156003f1e6c76f6aaf425d
krishnanIT <bk111100001100@gmail.com>
2025-03-11 13:09:20 +0530

    second time push

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.calculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,7 +17,20 @@ class MainActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+            insets }
+
+        //write the application code hear.
+       //al cal_second_screen = findViewById<TextView>(R.id.screen)
+        val cal_screen = findViewById<EditText>(R.id.calculator_screen)
+        val b_00 = findViewById<Button>(R.id.double_zero)
+        val b_0 = findViewById<Button>(R.id.zero)
+        val b_1= findViewById<Button>(R.id.one)
     }
+
+
+
 }
+
+
+
+
